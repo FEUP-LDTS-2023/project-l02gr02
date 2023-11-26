5bfdaf9db609129d964a421818b9471736ebced9
Afonso-Machado <up202207611@up.pt>
2023-11-26 13:18:22 +0000

    Added Needed Resources

--- src/main/java/com/gr02/BomberMania/viewer/menu/MenuViewer.java
+++ src/main/java/com/gr02/BomberMania/viewer/menu/MenuViewer.java
@@ -12,7 +12,7 @@ public class MenuViewer extends Viewer<Menu> {
 
     @Override
     protected void drawElements(GUI gui) {
-        gui.drawText(new Position(5, 5), "Menu", "#FFFFFF");
+        gui.drawText(new Position(5, 5), "BomberMania", "#FFFFFF");
 
         for (int i = 0; i < getModel().getNumberEntries(); i++) {
             gui.drawText(new Position(5, 7+i), getModel().getEntry(i), getModel().isSelected(i) ? "#FFD700" : "#FFFFFF");
